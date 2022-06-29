f4c3dfedcec4cec270614e7b2ffc00439450be07
Joshua Dane <joshrdane@gmail.com>
2022-06-29 11:34:54 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
